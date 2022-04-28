a4a3e537523f93e8671cb3f17a461c819e3e3043
William Gouvea <william.gouvea.c@conceptatech.com>
2022-04-28 14:55:51 -0300

    - First commit Bytebank (Curso Flutter crie seu primeiro app)

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Bytebank());
+}
+
+class Bytebank extends StatelessWidget {
+  const Bytebank({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.greenAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
